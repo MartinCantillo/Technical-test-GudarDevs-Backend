0cb0d9c4b1f98518230e1daaf4b44e67a7f16ebf
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-04-11 20:20:18 -0500

    config jwt in the conteiner

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using RepositoriesIAdditionalField.IAdditionalField;
 using RepositoriesIAuthenticate.IAuthenticate;
 using RepositoriesIContact.IContact;
@@ -15,6 +18,42 @@ builder.Services.AddScoped<IAuthenticate, SAuthenticate>();
 builder.Services.AddScoped<IAdditionalField, SAdditionalField>();
 builder.Services.AddScoped<IContact, SContact>();
 builder.Services.AddScoped<IContactType, SContactType>();
+
+//config of jwt 
+
+builder.Configuration.AddJsonFile("appsettings.json");
+string? secretkey = builder.Configuration.GetSection("settings").GetSection("secretkey").ToString();
+var keyBytes = Encoding.UTF8.GetBytes(secretkey ?? "");
+builder.Services.AddAuthentication(config =>
+{
+
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+
+    config.RequireHttpsMetadata = false;
+
+    config.SaveToken = true;
+
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+
+        ValidateIssuerSigningKey = true,
+
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+
+        ValidateIssuer = true,
+        ValidIssuer = "backend",
+
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+    }
+;
+});
+
 
 
 builder.Services.AddControllers();
